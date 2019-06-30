bac4553426b30516d70520c2c1dec8bc3add98d0
Banooon Saleh <banoonsaleh@icloud.com>
2019-06-30 19:29:44 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Banooon Saleh on 27/10/1440 AH.
+//  Copyright © 1440 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
